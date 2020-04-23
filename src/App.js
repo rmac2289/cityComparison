775655ed05a62d5faba77aaf35e9ff095fac30f4
Ross MacDonald <rmac2289@gmail.com>
2020-04-22 17:24:43 -0700

    added tabs

--- src/App.js
+++ src/App.js
@@ -8,21 +8,22 @@ import CityName from './CityName'
 import MapContainer from './Map'
 import Footer from './Footer'
 import CityImage from './CityImage'
+import { Tabs, Tab } from 'react-bootstrap'
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-  this.state = {
-    city: "",
-    nameScore: [],
-    cityScore: "",
-    cityName: "",
-    webPhoto: null,
-    mobilePhoto: null,
-    latLong: {}
+    this.state = {
+      city: "",
+      nameScore: [],
+      cityScore: "",
+      cityName: "",
+      webPhoto: null,
+      mobilePhoto: null,
+      latLong: {}
+    }
   }
-}
   resetList = () => {
     this.setState({
       nameScore: [],
@@ -37,144 +38,158 @@ class App extends Component {
       city: event.target.value
     });
   }
-  
+
   handleSubmit = (event) => {
     event.preventDefault();
     this.resetList();
-  const options =  {
-    method: 'GET',
-    headers: {
-      "Content-Type": "application/json",
-    }
-  }
-  let uaAPI;
-  let scoresAPI;
-  let scores;
-  let image;
-  let coordinates;
-  let wrong = "Something went wrong, please try again later"
-  fetch(`https://api.teleport.org/api/cities/?search=${this.state.city}`,options)
-  .then(response => {
-    if(!response.ok) {
-      throw new Error(wrong);
-    }
-    return response.json();
-  })
-  .then(data => {
-    this.setState({NoResult: data.count})
-    this.state.NoResult === 0 ? alert("sorry, that city isn't in our system"):
-    console.log(data)
-    let searchResults = "city:search-results"
-    uaAPI = data._embedded[searchResults][0]._links["city:item"].href;
-    return fetch(uaAPI,options)
-  })
-    .then(response => {
-      if(!response.ok){
-        throw new Error(wrong);
+    const options = {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
       }
-      return response.json();
-    })
-    .then(newData => {
-      scoresAPI = newData._links["city:urban_area"].href
-      return fetch(scoresAPI,options)
+    }
+    let uaAPI;
+    let scoresAPI;
+    let scores;
+    let image;
+    let coordinates;
+    let wrong = "Something went wrong, please try again later"
+    fetch(`https://api.teleport.org/api/cities/?search=${this.state.city}`, options)
       .then(response => {
-        if(!response.ok){
+        if (!response.ok) {
           throw new Error(wrong);
         }
         return response.json();
       })
-      .then(newerData => {
-        coordinates = newerData.bounding_box.latlon
-        this.setState({
-          cityName: [...this.state.cityName, newerData.full_name],
-          latLong: {west: coordinates.west, north: coordinates.north, south: coordinates.south, east: coordinates.east}})
-        console.log(newerData)
-        image = newerData._links["ua:images"].href
-        scores = newerData._links["ua:scores"].href
-        return fetch(scores, options)
-        .then(response => {
-          if (!response.ok){
-            throw new Error(wrong);
-          }
-          return response.json()
-        })
-        .then(scoreData => {
-          let scores = scoreData.categories;
-
-            this.setState({
-              nameScore: [...this.state.nameScore, ...scores],
-              cityScore: [...this.state.cityScore, scoreData.teleport_city_score]
-          });
-          return fetch(image, options)
+      .then(data => {
+        this.setState({ NoResult: data.count })
+        this.state.NoResult === 0 ? alert("sorry, that city isn't in our system") :
+          console.log(data)
+        let searchResults = "city:search-results"
+        uaAPI = data._embedded[searchResults][0]._links["city:item"].href;
+        return fetch(uaAPI, options)
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(wrong);
+        }
+        return response.json();
+      })
+      .then(newData => {
+        scoresAPI = newData._links["city:urban_area"].href
+        return fetch(scoresAPI, options)
           .then(response => {
-            if (!response.ok){ 
-              throw new Error(wrong)
+            if (!response.ok) {
+              throw new Error(wrong);
             }
-            return response.json()
+            return response.json();
           })
-          .then(imageLink => {
+          .then(newerData => {
+            coordinates = newerData.bounding_box.latlon
             this.setState({
-              webPhoto: imageLink.photos[0].image.web,
-              mobilePhoto: imageLink.photos[0].image.mobilePhoto
+              cityName: [...this.state.cityName, newerData.full_name],
+              latLong: { west: coordinates.west, north: coordinates.north, south: coordinates.south, east: coordinates.east }
             })
-            
+            console.log(newerData)
+            image = newerData._links["ua:images"].href
+            scores = newerData._links["ua:scores"].href
+            return fetch(scores, options)
+              .then(response => {
+                if (!response.ok) {
+                  throw new Error(wrong);
+                }
+                return response.json()
+              })
+              .then(scoreData => {
+                let scores = scoreData.categories;
+
+                this.setState({
+                  nameScore: [...this.state.nameScore, ...scores],
+                  cityScore: [...this.state.cityScore, scoreData.teleport_city_score]
+                });
+                return fetch(image, options)
+                  .then(response => {
+                    if (!response.ok) {
+                      throw new Error(wrong)
+                    }
+                    return response.json()
+                  })
+                  .then(imageLink => {
+                    this.setState({
+                      webPhoto: imageLink.photos[0].image.web,
+                      mobilePhoto: imageLink.photos[0].image.mobilePhoto
+                    })
+
+                  })
+              })
           })
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
         })
       })
-    })
-    .catch(err => {
-    this.setState({
-      error: err.message
-    })
-  })
-}
-  render(){
-    
-   const north = this.state.latLong.north
-   const south = this.state.latLong.south
-   const east = this.state.latLong.east
-   const west = this.state.latLong.west
-    const lat = (north+south)/2
-    const long = (east+west)/2
+  }
+  render() {
+
+    const north = this.state.latLong.north
+    const south = this.state.latLong.south
+    const east = this.state.latLong.east
+    const west = this.state.latLong.west
+    const lat = (north + south) / 2
+    const long = (east + west) / 2
     const scoreItems = this.state.nameScore
-    const scoresList = scoreItems.map((value,index) => {
+    const scoresList = scoreItems.map((value, index) => {
       return <Scores key={index}
         name={value.name}
         score={value.score_out_of_10}
-        />
+      />
     })
 
-  return (
-    <div className="App">
-      <Nav />
-      <Header />
-      <SearchBar 
-        cityChanged={this.cityChanged}
-        handleSubmit={this.handleSubmit}
-        value={this.state.city}/>
-      <MapContainer
-        north={north} 
-        west={west} 
-        south={south} 
-        east={east} 
-        lattitude={lat} 
-        longitude={long} />
-      <CityName 
-        cityName={this.state.cityName} 
-        score={this.state.cityScore}/>
-        
-      <CityImage 
-      image={this.state.webPhoto}
-      hidden={this.state.webPhoto}
-      />  
-      {this.state.cityName !== '' &&
-      <ul className="mainList">
-      {scoresList}
-      </ul>}
-      <Footer />
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <Nav />
+        <Header />
+        <SearchBar
+          cityChanged={this.cityChanged}
+          handleSubmit={this.handleSubmit}
+          value={this.state.city} />
+        <MapContainer
+          north={north}
+          west={west}
+          south={south}
+          east={east}
+          lattitude={lat}
+          longitude={long} />
+        <CityName
+          cityName={this.state.cityName}
+          score={this.state.cityScore} />
+        {this.state.cityName !== '' &&
+          <Tabs className="tabs" defaultActiveKey="scores" id="noanim-tab-example">
+            <Tab eventKey="scores" title="Scores">
+              <CityImage
+                image={this.state.webPhoto}
+                hidden={this.state.webPhoto}
+              />
+              <ul className="mainList">
+                {scoresList}
+              </ul>
+            </Tab>
+            <Tab eventKey="profile" title="Profile">
+              <h2>page 2</h2>
+            </Tab>
+            <Tab eventKey="contact" title="Contact">
+              <h2>page 3</h2>
+            </Tab>
+            <Tab eventKey="contact" title="Contact">
+              <h2>page 4</h2>
+            </Tab>
+          </Tabs>}
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -2,196 +2,18 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import ApiKey from './ApiKey';
 import './Map.css'
+import styles from './MapStyles'
 
-const mapStyles = {
-    height: "100%",
-    width: "100%",
-    position: 'static !important',
 
-};
-const styles = [
-    {
-        "featureType": "all",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "weight": "2.00"
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "geometry.stroke",
-        "stylers": [
-            {
-                "color": "#9c9c9c"
-            }
-        ]
-    },
-    {
-        "featureType": "all",
-        "elementType": "labels.text",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#f2f2f2"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 45
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#eeeeee"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#7b7b7b"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    },
-    {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road.arterial",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "all",
-        "stylers": [
-            {
-                "color": "#46bcec"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#c8d7d4"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.fill",
-        "stylers": [
-            {
-                "color": "#070707"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "elementType": "labels.text.stroke",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
-    }
-]
 
 export class MapContainer extends Component {
 
     shouldComponentUpdate() {
-        return this.props.east === undefined ? false : true
-    }
-
+        return this.props.east === undefined ? false : true}
     render() {
-
         const ne = new this.props.google.maps.LatLng({ lat: this.props.north, lng: this.props.east })
         const sw = new this.props.google.maps.LatLng({ lat: this.props.south, lng: this.props.west })
         const bounds = new this.props.google.maps.LatLngBounds(sw, ne);
-
 
         return (
             <div id="mapContainer">
@@ -199,7 +21,7 @@ export class MapContainer extends Component {
                     google={this.props.google}
                     centerAroundCurrentLocation={true}
                     zoom={12}
-                    style={mapStyles}
+                    style={styles.mapStyles}
                     styles={styles}
                     center={{
                         lat: this.props.lattitude,
@@ -207,7 +29,6 @@ export class MapContainer extends Component {
                     }}
                     bounds={bounds}>
                 </Map>
-
             </div>
         );
     }
