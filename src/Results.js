655e4878d91bd52bf6e292d803e23f535637f31a
Ross MacDonald <rmac2289@gmail.com>
2020-09-12 03:31:04 -0700

    formatted custom tooltip for chart

--- src/Results.js
+++ src/Results.js
@@ -6,20 +6,40 @@ import { ResponsiveContainer, BarChart, Tooltip, Bar, XAxis, YAxis } from 'recha
 
 export default function TabCategories(props) {
     const salaries = props.salaryList
-    const data = salaries.map((v,i) => {
+    const data = salaries.map(v => {
         const toStr = v.salary_percentiles.percentile_75.toString()
         const lowToStr = v.salary_percentiles.percentile_25.toString()
         const slicedNum = toStr[5] === "." ? toStr.slice(0,5):toStr.slice(0,6);
-        const slicedLow = toStr[5] === "." ? lowToStr.slice(0,5):lowToStr.slice(0,6)
+        const slicedLow = lowToStr[5] === "." ? lowToStr.slice(0,5):lowToStr.slice(0,6);
         return {
             "name": v.job.title,
             "Salary": [slicedLow, slicedNum]
         }
-    })
+    });
+    const CustomTooltip = ({ active, payload, label }) => {
+        if (active) {
+            let lowPayloadFormatted;
+            let highPayloadFormatted;
+            const lowArray = payload[0].value[0].split('')
+            const highArray = payload[0].value[1].split('')
+
+            lowArray.length === 5 ? lowArray.splice(2, 0, ','):lowArray.splice(3, 0, ',')
+            highArray.length === 5 ? highArray.splice(2, 0, ','): highArray.splice(3, 0, ',')
+            lowPayloadFormatted = lowArray.join('')
+            highPayloadFormatted = highArray.join('')    
+          return (
+            <div className="custom-tooltip">
+              <p className="label-1">{label}</p>
+              <p className="intro-1">{payload[0].name}: ${lowPayloadFormatted} - ${highPayloadFormatted}</p>
+            </div>
+          );
+        }
+        return null;
+    }
     return (
-        <div className="results" defaultActiveKey="scores" id="noanim-tab-example">
+        <div className="results">
             
-            <div eventKey="scores" title="Scores">
+            <div>
                 <CityImage
                     image={ props.webPhoto }
                     hidden={ props.webPhoto }
@@ -32,12 +52,12 @@ export default function TabCategories(props) {
             <div className="subheader-box">
                 <h2 className="subheader">Job Market</h2>
             </div>
-            <div className="salaries" eventKey="Salaries" title="Salaries">
+            <div className="salaries">
                 <div className="chart-box">
                     <h2 id="salary-header">estimated yearly salary ($)</h2>
                 <ResponsiveContainer  width='100%' minHeight={1600}>
                     <BarChart margin={{top: 0}} barCategoryGap="15%" height={1600} data={data} layout="vertical">
-                        <Tooltip />
+                        <Tooltip offset={0} wrapperStyle={{ top: -75, left: 100, width: "250px" }} content={<CustomTooltip/>} />
                         <XAxis label={{fill: "rgb(0,0,0,0.8)"}} domain={[0,300000]}orientation="top" type="number"/>
                         <YAxis tickLine={false} width={190} axisLine={false} type="category" dataKey="name"/>
                         <Bar background={true} fill="rgba(100,0,0,0.6)" dataKey="Salary" />
